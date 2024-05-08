64026023addff7564538cfc760a4857e3a4daacb
AIbertWesker <mbubel999@gmail.com>
2024-05-08 16:23:28 +0200

    add room database to code

---
+++ app/src/main/java/com/example/meetingapp/database/MeetingDatabase.kt
@@ -0,0 +1,38 @@
+package com.example.meetingapp.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.meetingapp.CustomOptions
+import com.example.meetingapp.Meetings
+import com.example.meetingapp.TemplateCustomOptions
+import com.example.meetingapp.Templates
+
+@Database(entities = [Meetings::class, CustomOptions::class, Templates::class, TemplateCustomOptions::class], version = 1)
+abstract class MeetingDatabase : RoomDatabase(){
+
+    abstract fun getMeetingDoa(): MeetingDao
+    abstract fun getTemplateDao(): TemplateDao
+
+    companion object{
+        @Volatile
+        private var instance: MeetingDatabase? = null
+        private val LOCK = Any()
+
+        open fun invoke(context: Context) = instance ?:
+        synchronized(LOCK){
+            instance ?:
+            createDatabase(context).also{
+                instance= it
+            }
+        }
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                MeetingDatabase::class.java,
+                "meeting_db"
+            ).build()
+    }
+}
---
+++ app/src/main/java/com/example/meetingapp/database/TemplateDao.kt
@@ -0,0 +1,36 @@
+package com.example.meetingapp.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.meetingapp.TemplateCustomOptions
+import com.example.meetingapp.TemplateWithTemplateOptions
+import com.example.meetingapp.Templates
+
+interface TemplateDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertTemplate(templates: Templates)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertTemplateOption(templateCustomOptions: TemplateCustomOptions)
+
+    @Update
+    suspend fun updateTemplate(templates: Templates)
+    @Update
+    suspend fun updateTemplateOption(templateCustomOptions: TemplateCustomOptions)
+
+    @Delete
+    suspend fun deleteTemplate(templates: Templates)
+
+    @Delete
+    suspend fun deleteTemplateOption(templateCustomOptions: TemplateCustomOptions)
+
+    @Query("SELECT * FROM TEMPLATES ORDER BY templateId DESC")
+    fun getAllTemplates(): LiveData<List<TemplateWithTemplateOptions>>
+
+    @Query("SELECT * FROM TEMPLATEOPTIONS")
+    fun getAllTemplateOptions(): LiveData<List<TemplateCustomOptions>>
+}
