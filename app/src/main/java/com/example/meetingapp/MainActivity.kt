77efdb549a1d68540f4fd0f7214b4ef933b59ed8
AIbertWesker <mbubel999@gmail.com>
2024-05-11 17:44:27 +0200

    Added meetingviewmodel && factory

--- app/src/main/java/com/example/meetingapp/MainActivity.kt
+++ app/src/main/java/com/example/meetingapp/MainActivity.kt
@@ -5,8 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.meetingapp.ViewModels.MeetingViewModel
+import com.example.meetingapp.ViewModels.ViewModelFactory
+import com.example.meetingapp.database.MeetingDatabase
+import com.example.meetingapp.repository.MeetingRepository
 
 class MainActivity : AppCompatActivity() {
+
+        private val viewModel: MeetingViewModel by lazy{
+            ViewModelProvider(
+                this,
+                ViewModelFactory(MeetingRepository(MeetingDatabase.invoke(this)))
+            ).get(MeetingViewModel::class.java)
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +28,9 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        viewModel.getAllMeetings().observe(this){
+            print(it)
         }
     }
 }
